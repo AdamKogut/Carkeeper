bdbc2d25b912d50c69a2c36b04b795cde4a5d0d1
aswami98 <aswami@purdue.edu>
2018-04-09 02:24:16 -0400

    set up firebase and made createUser function for database

---
+++ database.js
@@ -0,0 +1,21 @@
+// Create Functions
+
+function createUser(userRef, id, email, firstname, lastname, phone, password) {
+  userRef.update({
+    [id]:"novalue"
+  });
+  userRef.child(id).update({
+    "email": email,
+    "firstname": firstname,
+    "lastname": lastname,
+    "phone": phone,
+    "password": password
+  });
+}
+
+
+// Read Functions
+
+
+
+// Update Functions
