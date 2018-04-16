25d6be56dbd2f7da22504889ca694447b094b8c9
AdamKogut <akogut@purdue.edu>
2018-04-16 15:32:44 -0400

    Add change forgot password page

--- frontend/src/Home/HomeLayout.js
+++ frontend/src/Home/HomeLayout.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import HomeTopbar from './TopBar/HomeTopbar'
 import HomeBody from './HomeBody'
+import ForgotPasswordTargetPage from './TopBar/ForgotPasswordTargetPage';
 //import './HomeLayout.css';
 
 class HomeLayout extends Component {
   render() {
     return (
       <div>
+        <ForgotPasswordTargetPage {...this.props} />
         <HomeTopbar {...this.props} />
         <HomeBody />
       </div>
